9eb2d7314dbd86a44161358bf36875eb99cb17e8
Fev1L <demchvlad17@gmail.com>
2026-03-25 23:24:45 +0100

    V0.0.2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,51 @@
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
+#define SDL_MAIN_USE_CALLBACKS 1
 
-    return 0;
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+#include "types.h"
+
+//=================================================================
+SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[]){
+    App* app = new App();
+
+    if (!SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO)) {
+        SDL_Log("SDL Init error: %s", SDL_GetError());
+        return SDL_APP_FAILURE;
+    }
+
+    app->window = SDL_CreateWindow("PLUA", 700, 700, SDL_WINDOW_OPENGL);
+    app->renderer = SDL_CreateRenderer(app->window, nullptr);
+
+    *appstate = app;
+    return SDL_APP_CONTINUE;
 }
+//=================================================================
+SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
+{
+
+    if (event->type == SDL_EVENT_QUIT) return SDL_APP_SUCCESS;
+
+    if (event->type == SDL_EVENT_KEY_DOWN)
+    {
+        SDL_Keycode key = event->key.key;
+        if (key == SDLK_ESCAPE) return SDL_APP_SUCCESS;
+    }
+
+    return SDL_APP_CONTINUE;
+}
+//=================================================================
+SDL_AppResult SDL_AppIterate(void* appstate)
+{
+
+    return SDL_APP_CONTINUE;
+}
+//=================================================================
+void SDL_AppQuit(void* appstate, SDL_AppResult result)
+{
+
+    SDL_Quit();
+}
+//=================================================================
---
+++ types.h
@@ -0,0 +1,14 @@
+//
+// Created by Fev1L on 25.03.2026.
+//
+
+#ifndef PLUA_TYPES_H
+#define PLUA_TYPES_H
+
+struct App
+{
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
+
+#endif //PLUA_TYPES_H
